16d4f7b722889c4bd13e61ad4137443f9c7a9bf5
olsonek <edward.k.olson@gmail.com>
2015-10-15 22:09:32 -0400

    Added a few TODOs to remind me to rewrite core methods to fit the new core specification once I'm done rewriting the main function specs.

--- src/core.js
+++ src/core.js
@@ -2,22 +2,29 @@
  * Created by Eddie on 10/14/2015.
  */
 import {List, Map, Set, fromJS} from 'immutable';
-import {getPlayer, setPlayer, getWorker, setWorker} from './internal';
+import {getPlayer, getWorker, setWorker} from './internal';
 
+// TODO: Rewrite core methods to fit the spec.
+// TODO: Merge "internal" method dependencies into core methods via the new spec
 // TODO: Define metrics for valid usernames
-export function addPlayer(state, newPlayer) {
+export function setPlayer(state, player) {
     let players = state.get('players', List());
+    if(player.get('id')){
+        // update existing player
+    }else{
+        // create a new player
+    }
     var ids = Set();
-    var exists = players.some(function (player) {
+    var exists = players.some(function (p) {
         ids = ids.add(player.get('id'));
-        return player.get('username') === newPlayer.get('username');
+        return p.get('username') === player.get('username');
     });
     if (!exists) {
         var newId = 1;
         while (ids.has(newId)) {
             newId++;
         }
-        var addPlayer = newPlayer.set('id', newId);
+        var addPlayer = player.set('id', newId);
         return state.set('players', players.push(addPlayer));
     } else {
         return state;
@@ -72,15 +79,6 @@ export function setWorkerJob(state, playerId, workerId, job) {
     }
     return state;
 }
-
-
-
-
-
-
-
-
-
 
 
 // TODO: setWorkerTask, add to an action map
