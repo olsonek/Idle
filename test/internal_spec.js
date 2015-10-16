83c74eac59cabc70317e8bd330c9106413ce766c
olsonek <edward.k.olson@gmail.com>
2015-10-15 20:28:55 -0400

    Moved utility functions out of core.js and into internal.js.

---
+++ test/internal_spec.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Eddie on 10/15/2015.
+ */
+import {expect} from 'chai';
+import {List, Map, fromJS} from 'immutable';
+
+import {getPlayer, setPlayer, getWorker, setWorker} from '../src/internal';
+
+// TODO: Write unit tests for internal methods
