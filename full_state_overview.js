3b1c4ceb74ab76036dad689c4d386a14abdc2e42
olsonek <edward.k.olson@gmail.com>
2015-10-28 12:47:52 -0400

    Added descriptions of how everything is mapped together.

--- full_state_overview.js
+++ full_state_overview.js
@@ -2,11 +2,6 @@
  * Created by Eddie on 10/28/2015.
  */
 
-/**
- * Notes:
- * -The key for each collection except for players and tasks is a playerId
- */
-
 // TODO: Add state that maps jobs to industry
 // TODO: Add state that manages industry bonuses
 // TODO: Add state that maps tasks to jobs
@@ -15,23 +10,27 @@
 const serverState =
 {
     players: {
+        // username => playerId
         latestPlayerId: 3,
         'Eddie': '1',
         'Rob': '3'
     },
 
     playerData: {
+        // playerId => (username, misc player data)
         1: {username: 'Eddie', hairColor: 'Black'},
         3: {username: 'Rob', hairColor: 'Black', powerLevel: 'Over 9000'}
     },
 
     resources: {
+        // playerId => [(resource: amount)]
         1: {gold: 100, ore: 100},
         3: {money: 3.50, 'iron bars': 10}
     },
 
     workers: {
         // This state records primary information about the worker (name, job, and task).
+        // workers => playerId => workerId => (name, job, task)
         1: {
             latestWorkerId: 4,
             2: {name: 'Lucian', job: 'Miner', task: 'Break Rocks'},
@@ -58,6 +57,7 @@ const serverState =
     },
 
     tasks: {
+        // task => playerId => [workerId]
         'Mine Ore': {
             1: ['4']
         },
@@ -75,15 +75,18 @@ const clientState =
 {
     playerId: '3',
     playerData: {
+        // playerInfo => value
         username: 'Rob',
         hairColor: 'Black',
         powerLevel: 'Over 9000'
     },
     resources: {
+        // resource => amount
         money: 3.50,
         'iron bars': 10
     },
     workers: {
+        // workerId => (name, job, task)
         1: {name: 'Tonk'},
         2: {name: 'Alice'},
         3: {name: 'Jim', job: 'Miner', task: 'Break Rocks'},
